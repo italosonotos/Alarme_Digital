1f97a0e6d0c466c34010dc4733533086b9e4e092
italosonotos <107754956+italosonotos@users.noreply.github.com>
2023-03-21 17:24:09 -0300

    teste-003

--- src/script.js
+++ src/script.js
@@ -1,23 +1,26 @@
-function Relogio() {
 
+let tempo = document.getElementById('display');
+let alarme = document.getElementById('hora');
+let btn = document.getElementById('botão');
+let audio = document.getElementById('audio');
+
+function Relogio() {
 
     let data = new Date();
     let hora = data.getHours();
     let minuto = data.getMinutes();
     let segundos = data.getSeconds();
     let horaTotal = hora + ':' + minuto + ':' + segundos;
-    let tempo = document.getElementById('display');
+    let valorArlame = alarme.value;
 
     tempo.innerHTML = horaTotal
+
+    if(valorArlame === `${hora}:${minuto}`){
+        alarme = valorArlame; 
+        audio.play();       
+    }
 
 }
 
 setInterval(Relogio, 500);
 
-function acionarAlarme() {
-    let valorAlarme = document.getElementById('hora');
-
-
-    alert(`Hora Adiconada ${valorAlarme.value}`);
-
-}
